060b7253dca34ebec61a3617dc94765061b1c145
Roman Chaban <roman.chaban1001@gmail.com>
2024-10-02 23:27:42 -0800

    Styled default scrollbar

--- src/types/nav.ts
+++ src/types/nav.ts
@@ -1,3 +1,5 @@
+import { PATHS } from '@/constants/paths';
+
 export interface NavItem {
   id: number;
   icon: string;
@@ -11,43 +13,43 @@ export const nav: NavItems = [
   {
     id: 1,
     icon: '/icons/dashboardIcons/active.svg',
-    href: '/dashboard',
+    href: PATHS.HOME,
     children: 'Dashboard',
   },
   {
     id: 2,
     icon: '/icons/dashboardIcons/inactive.svg',
-    href: '/projects',
+    href: PATHS.PROJECTS,
     children: 'Projects',
   },
   {
     id: 3,
     icon: '/icons/dashboardIcons/calendar.svg',
-    href: '/calendar',
+    href: PATHS.CALENDAR,
     children: 'Calendar',
   },
   {
     id: 4,
     icon: '/icons/dashboardIcons/fly.svg',
-    href: '/vacations',
+    href: PATHS.VACATIONS,
     children: 'Vacations',
   },
   {
     id: 5,
     icon: '/icons/dashboardIcons/users.svg',
-    href: '/employees',
+    href: PATHS.EMPLOYEES,
     children: 'Employees',
   },
   {
     id: 6,
     icon: '/icons/dashboardIcons/chat.svg',
-    href: '/messenger',
+    href: PATHS.MESSENGER,
     children: 'Messenger',
   },
   {
     id: 7,
     icon: '/icons/dashboardIcons/folder.svg',
-    href: '/info',
+    href: PATHS.INFO_PORTAL,
     children: 'Info Portal',
   },
 ];
