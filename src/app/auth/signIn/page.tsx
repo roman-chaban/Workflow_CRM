133b0be68ae5b3965345843bee37bf101af2be57
Roman Chaban <roman.chaban1001@gmail.com>
2024-10-28 01:47:22 +0200

    Added new UI component CloseButton

--- src/app/auth/signIn/page.tsx
+++ src/app/auth/signIn/page.tsx
@@ -1,14 +1,10 @@
 import { Metadata } from "next";
 
-import { Button, SectionWrapper } from "@/components/index";
+import { SectionWrapper } from "@/components/index";
 import AuthLayout from "@/app/auth/layout";
 import { SignInBanner } from "@/components/SignInBanner/SignInBanner";
 import { SignInForm } from "@/components/SignInForm/SignInForm";
-
-import Link from "next/link";
-import Image from "next/image";
-
-import { PATHS } from "@/constants/paths";
+import { CloseButton } from "@/components/ui/buttons/CloseButton/CloseButton";
 
 import styles from "@/styles/pages/SignIn.module.scss";
 
@@ -23,17 +19,7 @@ export default function SignIn() {
         <div className={styles["signInContainer"]}>
           <SignInBanner />
           <SignInForm />
-          <Button type={"button"} className={styles["CloseButton"]}>
-            <Link href={PATHS.HOME}>
-              <Image
-                priority
-                src={"/images/signIn/close.svg"}
-                alt="Close Page Icon"
-                width={30}
-                height={30}
-              />
-            </Link>
-          </Button>
+          <CloseButton />
         </div>
       </SectionWrapper>
     </AuthLayout>
---
+++ src/components/ui/buttons/CloseButton/CloseButton.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import type { FC } from "react";
+
+import { PATHS } from "@/constants/paths";
+import { Button } from "@/components/ui/Button/Button";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "@/styles/pages/SignIn.module.scss";
+import useMediaQuery from "@/hooks/useMediaQuery";
+
+export const CloseButton: FC = () => {
+  const isTablet = useMediaQuery("(max-width: 1025px)");
+
+  return (
+    <Button
+      type={"button"}
+      className={`${styles["CloseButton"]} ${styles["CloseButtonPrimary"]}`}
+    >
+      <Link href={PATHS.HOME}>
+        {isTablet ? (
+          <Image
+            priority
+            src={"/images/signIn/blue-arrow.svg"}
+            alt="Close Page Icon"
+            width={30}
+            height={30}
+            className={styles["closeIconPrimary"]}
+          />
+        ) : (
+          <Image
+            priority
+            src={"/images/signIn/close.svg"}
+            alt="Close Page Icon"
+            width={30}
+            height={30}
+            className={styles["closeIcon"]}
+          />
+        )}
+      </Link>
+    </Button>
+  );
+};
